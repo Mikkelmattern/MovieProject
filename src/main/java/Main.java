1cec8a7ead69e53fd0e787d136bb67e9b5df21c7
Fabian-str <fabian.str@hotmail.com>
2026-09-19 09:43:14 +0200

    Added Search functionality by title and names.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,17 @@
 import jakarta.persistence.EntityManagerFactory;
+import mikkelmattern.DTO.MovieSearchResponseDTO;
+import mikkelmattern.DTO.PersonSearchResponseDTO;
 import mikkelmattern.config.HibernateConfig;
 import mikkelmattern.tmdb.TmdbClient;
+import mikkelmattern.utils.ConsolePrinter;
 
 public class Main {
 
     public static void main(String[] args) {
         TmdbClient client = new TmdbClient();
 
-        String json = client.get(
-                "/movie/11?language=en-US"
-        );
+        MovieSearchResponseDTO response = client.searchMovies("Star Wars");
 
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        ConsolePrinter.printMovieSearch(response);
     }
 }
